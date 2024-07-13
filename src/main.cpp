e8f47d65032776c3c68936d6042e61986822d180
1stub <stubbiegaming@gmail.com>
2024-07-13 13:29:09 -0400

    got imgui-sfml demo working in main currently,should be good to go

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
 #include <SFML/Graphics.hpp>
 #include "../debug/cpuUsage.hpp"
-#include "../include/imgui/imgui-master/imgui.h"
+#include "../include/imgui/imgui.h"
+#include "../include/imgui/imgui-SFML.h"
 #include "chunkManager.h"
 
 int main(){
   sf::RenderWindow window(sf::VideoMode(1600, 900), "Isometric", sf::Style::Close);
   window.setFramerateLimit(60);
+
+  ImGui::SFML::Init(window);
 
 	const siv::PerlinNoise::seed_type seed = 123456u;
 	const siv::PerlinNoise p{ seed };
@@ -15,21 +18,28 @@ int main(){
 
   chunkManager cm(p, window, v);
 
-  sf::Clock clock;
-  
+  sf::Clock deltaClock; 
   while(window.isOpen()){
     sf::Event e;
     while(window.pollEvent(e)){
+      ImGui::SFML::ProcessEvent(window, e);
+
       if(e.type == sf::Event::Closed){
         window.close();
       }
     }
-    sf::Time deltaTime = clock.restart();
+        ImGui::SFML::Update(window, deltaClock.restart());
 
-    window.clear();
+       // ImGui::ShowDemoWindow();
 
-    cm.update();
-    window.display();
+        ImGui::Begin("Hello, world!");
+        ImGui::Button("Look at this pretty button");
+        ImGui::End();
+
+        window.clear();
+        cm.update();
+        ImGui::SFML::Render(window);
+        window.display();
   }
   return 0;
 }
